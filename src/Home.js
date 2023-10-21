91660924f3bb58912e447bfe44c08c9c71f2a258
Jordan Altomare <jaltomar@u.rochester.edu>
2023-10-21 16:44:49 -0400

    added description of myself

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,8 @@ import useSound from "use-sound";
 import Button from "react-bootstrap/Button";
 import platesomeLogo from "./images/logo_platesome.png";
 import duck from "./images/duck.png";
+import jordan from "./images/jordan_pic.png";
+
 import duckSound from "./sounds/duckSound.mp3";
 
 export default function Home() {
@@ -12,17 +14,10 @@ export default function Home() {
 
   return (
     <>
-      <div
-        className="d-flex"
-        style={{ columnGap: "1rem", padding: ".5rem 1.5rem .5rem 1.5rem" }}
-      >
+      <div className="d-flex header">
         <h2 className="me-auto align-self-center mb-0">Jordan Altomare</h2>
 
-        <a
-          className="custom-link align-self-center"
-          style={{ display: "inline-block mt-4" }}
-          href="asdf"
-        >
+        <a className="custom-link align-self-center" href="asdf">
           GitHub
         </a>
         <a className="custom-link align-self-center" href="asdf">
@@ -31,43 +26,61 @@ export default function Home() {
       </div>
 
       <div className="d-flex p-4" style={{ columnGap: "1rem" }}>
-        <span>image</span>
+        <img width="30%" src={jordan}></img>
 
         <div>
           <h3>Who am I?</h3>
-          <p>loeiajfiaoj fdjiao fjdiao pjfdiao pjdifop ajiop ajfidoap j</p>
+          <p style={{ textAlign: "left" }}>
+            Some say I'm a myth. Others say I'm a ghost. In reality, I'm just a
+            man who inaccurately recalls other people's descriptions of me.
+          </p>
+          <p style={{ textAlign: "left" }}>
+            I obtained my B.S. in Computer Science from University of Rochester.
+            I have a passion for creating websites that feel as nice as they
+            look. I also very much enjoy doing mobile app development. Coding is
+            the most fun for me when I get to have some creative influence on
+            the outcome.
+          </p>
+          <p style={{ textAlign: "left" }}>
+            Outside of coding, I enjoy exercising, playing guitar, and listening
+            to The Beatles. Have you given them a listen? They're fantastic:{" "}
+            <a
+              href="https://www.youtube.com/watch?v=NCtzkaL2t_Y"
+              target="_blank"
+            >
+              Don't Let Me Down
+            </a>
+            .
+          </p>
         </div>
+      </div>
+      <div style={{ border: "red solid 0px" }} className="ms-auto">
+        <img
+          onClick={() => {
+            setDuckClickCount(duckClickCount + 1);
+            if (duckClickCount > 2) {
+              playSound();
+            }
+          }}
+          className="clickable"
+          width="30%"
+          src={duck}
+        ></img>
 
-        <span>imageTExt</span>
+        {duckClickCount > 0 && (
+          <p className="typewriter"> It appears to be a duck...</p>
+        )}
 
-        <div style={{ border: "red solid 2px" }} className="ms-auto">
-          <img
-            onClick={() => {
-              setDuckClickCount(duckClickCount + 1);
-              if (duckClickCount > 2) {
-                playSound();
-              }
-            }}
-            className="clickable"
-            width="30%"
-            src={duck}
-          ></img>
+        {duckClickCount > 1 && (
+          <p className="typewriter">
+            Maybe if you click it again it will quack...
+          </p>
+        )}
+        {duckClickCount > 2 && (
+          <p className="typewriter">Hmmm... One more time perhaps?</p>
+        )}
 
-          {duckClickCount > 0 && (
-            <p className="typewriter"> It appears to be a duck...</p>
-          )}
-
-          {duckClickCount > 1 && (
-            <p className="typewriter">
-              Maybe if you click it again it will quack...
-            </p>
-          )}
-          {duckClickCount > 2 && (
-            <p className="typewriter">Hmmm... One more time perhaps?</p>
-          )}
-
-          {duckClickCount > 3 && <p className="typewriter">Amazing!</p>}
-        </div>
+        {duckClickCount > 3 && <p className="typewriter">Amazing!</p>}
       </div>
       <div class="rotating-image"></div>
 
