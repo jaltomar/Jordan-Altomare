252e54c87ff04501bb201123da952e67f7f40533
Jordan Altomare <jaltomar@u.rochester.edu>
2023-10-22 21:44:56 -0400

    got rough layout for icons

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,19 @@ import React, { useState } from "react";
 import useSound from "use-sound";
 
 import Button from "react-bootstrap/Button";
-import platesomeLogo from "./images/logo_platesome.png";
+import platesomeLogo from "./images/platesome7.png";
 import duck from "./images/duck.png";
 import jordan from "./images/jordan_pic.png";
-import quikwiz from "./images/quikwiz5.png";
-import tinyMiracle from "./images/tinyMiracle.png";
-import ezLearn from "./images/ezLearnLogo.png";
+import quikwiz from "./images/quikwizlogo6.png";
+import tinyMiracle from "./images/tinyMiracle6.png";
+import ezLearn from "./images/ezLearnLogo6.png";
 
 import duckSound from "./sounds/duckSound.mp3";
 
 export default function Home() {
   const [duckClickCount, setDuckClickCount] = useState(0);
+  const [hoverBox, setHoverBox] = useState(-1);
+
   const [playSound] = useSound(duckSound);
 
   return (
@@ -29,7 +31,7 @@ export default function Home() {
       </div>
 
       <div className="d-flex p-4" style={{ columnGap: "1rem" }}>
-        <img alt="Jordan's photo" width="30%" src={jordan}></img>
+        <img alt="Jordan's photo" width="25%" src={jordan}></img>
 
         <div>
           <h3>Who am I?</h3>
@@ -40,9 +42,9 @@ export default function Home() {
           <p style={{ textAlign: "left" }}>
             I obtained my B.S. in Computer Science from University of Rochester.
             I have a passion for creating websites that feel as nice as they
-            look. I also very much enjoy doing mobile app development. Coding is
-            the most fun for me when I get to have some creative influence on
-            the outcome.
+            look. I also enjoy doing mobile app development. Coding is the most
+            fun for me when I get to have some creative influence on the
+            outcome.
           </p>
           <p style={{ textAlign: "left" }}>
             Outside of coding, I enjoy exercising, playing guitar, and listening
@@ -58,33 +60,79 @@ export default function Home() {
         </div>
       </div>
       <h3>My Creations</h3>
-
-      <div className="d-flex">
-        <div
-          className="rotating container"
-          style={{ border: "red solid 0px", width: "25%" }}
-        >
-          <img alt="Tiny Miracles Rescue logo" width="100%" src={tinyMiracle} />
+      <div className="p-4">
+        <div className="d-flex" style={{ border: "red solid 0px" }}>
+          <div className="rotating container" style={{ width: "50%" }}>
+            <img
+              style={{ marginLeft: "-20px", marginRight: "20px" }}
+              alt="Tiny Miracles Rescue logo"
+              width="50%"
+              src={tinyMiracle}
+            />
+            <div>
+              <h3>Tiny Miracles</h3>
+              <p className="visible-text">
+                Quikwiz is a fun trivia game created with React. Users are asked
+                old questions from the show Jeopardy! See how many questions you
+                can get correct!
+              </p>
+            </div>
+          </div>
+          <div
+            onMouseEnter={() => {
+              setHoverBox(2);
+            }}
+            className="rotating container"
+            style={{ width: "50%" }}
+          >
+            <img alt="Platesome logo" width="50%" src={platesomeLogo} />
+            <div>
+              <h3>Platesome</h3>
+              <p className="visible-text">
+                Quikwiz is a fun trivia game created with React. Users are asked
+                old questions from the show Jeopardy! See how many questions you
+                can get correct!
+              </p>
+            </div>
+          </div>
         </div>
-        <div
-          className="rotating container"
-          style={{ border: "red solid 0px", width: "25%" }}
-        >
-          <img alt="Platesome logo" width="100%" src={platesomeLogo} />
-        </div>
-        <div
-          className="rotating container"
-          style={{ border: "red solid 0px", width: "25%" }}
-        >
-          <img alt="EZLearn GuitAR logo" width="100%" src={ezLearn} />
-        </div>
-        <div
-          className="rotating container"
-          style={{ border: "red solid 0px", width: "25%" }}
-        >
-          <img alt="Quikwiz logo" width="100%" src={quikwiz} />
+        <div className="d-flex" style={{ border: "red solid 0px" }}>
+          <div className="rotating container" style={{ width: "50%" }}>
+            <img alt="EZLearn GuitAR logo" width="50%" src={ezLearn} />
+            <div>
+              <h3>EZLearn GuitAR</h3>
+              <p className="visible-text">
+                Quikwiz is a fun trivia game created with React. Users are asked
+                old questions from the show Jeopardy! See how many questions you
+                can get correct!
+              </p>
+            </div>
+          </div>
+          <div
+            onMouseEnter={() => {
+              setHoverBox(1);
+            }}
+            onMouseLeave={() => {
+              // setHoverBox(-1);
+            }}
+            className="rotating container"
+            style={{ width: "50%" }}
+          >
+            <img alt="Quikwiz logo" width="50%" src={quikwiz} />
+            <div>
+              <h3>Quikwiz</h3>
+              {hoverBox > -3 && (
+                <p className="visible-text">
+                  Quikwiz is a fun trivia game created with React. Users are
+                  asked old questions from the show Jeopardy! See how many
+                  questions you can get correct!
+                </p>
+              )}
+            </div>
+          </div>
         </div>
       </div>
+
       <div style={{ border: "red solid 0px" }} className="me-auto">
         <img
           onClick={() => {
